554010b7e29f41fa069ed745ec2b0164953ca8eb
TobiasPettersson <pettersson.tobias84@gmail.com>
2017-01-26 15:46:40 +0100

    Updates

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
+  $(".menulinks").click(function(){
+    $(this).addClass("listClicked");
+  });
 
+
+ $(".menulinks").mouseenter(function(){
+   $(this).animate({width: '100%'}, 1000);
+   $(this).css("background-color", 'rgba(0, 0, 0, 0.5)');
+   $(this).css('color', 'hotpink');
+
+  });
+  $(".menulinks").mouseleave(function(){
+    if($(this).hasClass("listClicked")){
+    }
+    else {
+      $(this).animate({width: '80%'}, 1000);
+      $(this).css("background-color", 'rgba(0, 0, 0, 0.2)');
+      $(this).css('color', 'white');
+    }
+  });
 });
